a9b90c7d312172ae6c048e469f1decd3a50eb7a5
otter254 <70493770+otter254@users.noreply.github.com>
2022-03-13 16:30:37 +0900

    swiper

--- index.js
+++ index.js
@@ -228,20 +228,3 @@ for (let i = 0; i < linkElem.length; i++) {
     });
 }
 
-// swiper
-var swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
