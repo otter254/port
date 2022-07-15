e8c4b4a9e24125872421c76488f4938961b57907
otter254 <70493770+otter254@users.noreply.github.com>
2022-07-16 01:20:32 +0900

    image filter

--- js/product.js
+++ js/product.js
@@ -55,3 +55,26 @@ document.addEventListener("scroll", function() {
     }
   }
 })
+
+const filterItem = document.querySelector(".items");
+const filterImg = document.querySelectorAll(".galley-item");
+
+window.onload = ()=>{
+  filterItem.onclick = (selectedItem)=>{
+    if(selectedItem.target.classList.contains("item")){
+      filterItem.querySelector(".active").classList.remove("active");
+      selectedItem.target.classList.add("active");
+      let filterName = selectedItem.target.getAttribute("data-name");
+      filterImg.forEach((image)=>{
+        let filterImages = image.getAttribute("data-name");
+        if((filterImages == filterName) || filterName == "all"){
+          image.classList.remove("hide");
+          image.classList.add("itemshow");
+        }else {
+          image.classList.add("hide");
+          image.classList.remove("itemshow");
+        }
+      });
+    }
+  }
+}
