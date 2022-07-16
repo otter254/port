a2f31adace3c052765ab5294b6e1f2e466816d7d
otter254 <70493770+otter254@users.noreply.github.com>
2022-07-16 11:27:03 +0900

    下層swiper

--- js/swiper.js
+++ js/swiper.js
@@ -16,4 +16,33 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-console.log('aaa')
+
+const mySwiper_thumb = new Swiper('.work_swiper .swiper-thumb', {
+  slidesPerView: 5,
+  spaceBetween: 8,
+  grabCursor: true,
+  scrollbar: {
+    el: '.work_swiper .swiper-scrollbar',
+    draggable: true,
+  },
+  breakpoints: {
+    600: {
+      slidesPerView: 7,
+    },
+  },
+});
+
+const mySwiper_main = new Swiper('.work_swiper .swiper-main', {
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+  speed: 500,
+  navigation: {
+    nextEl: '.work_swiper .swiper-button-next',
+    prevEl: '.work_swiper .swiper-button-prev',
+  },
+  thumbs: {
+    swiper: mySwiper_thumb,
+  },
+});
